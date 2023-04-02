299a6787005f4c99616816c836d0ab08aae234ae
Akshitth V. Ravichandran <akshitth723@gmail.com>
2023-04-02 15:59:48 +0300

    replaced react class components with hooks

--- screens/OnboardingScreen.js
+++ screens/OnboardingScreen.js
@@ -1,53 +1,29 @@
-import React from "react";
-import { View, ScrollView, TouchableWithoutFeedback, KeyboardAvoidingView, Keyboard, Text, TextInput, Pressable } from "react-native";
+import { useEffect, useState } from "react";
+import { View, Text, TextInput, Pressable } from "react-native";
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
 
 import styles from '../styles';
 
-export default class OnboardingScreen extends React.Component {
-  state = {
-    firstName: '',
-    email: '',
-    buttonPressed: false,
-    inputValid: false
-  }
+export default function OnboardingScreen(props) {
+  const [firstName, setFirstName] = useState();
+  const [email, setEmail] = useState();
+  const [buttonPressed, setButtonPressed] = useState(false);
+  const [inputValid, setInputValid] = useState(false);
 
-  componentDidUpdate(prevProps, prevState) {
-    if (this.state.firstName !== prevState.firstName || this.state.email !== prevState.email) {
-      this.setState({
-        inputValid: false
-      })
+  useEffect(() => {
+    setInputValid(validateInput());
+  }, [firstName, email]);
 
-      if (this.validateInput()) {
-        this.setState({
-            inputValid: true
-        })
-      }
-    }
-  }
+  const handleButtonPressIn = () => {
+    setButtonPressed(true);
+  };
 
-  handleNameChange = (firstName) => {
-    this.setState({firstName})
-  }
-
-  handleEmailChange = (email) => {
-    this.setState({email})
-  }
-
-  handleButtonPressIn = () => {
-    this.setState({
-      buttonPressed: true
-    })
-  }
-
-  handleButtonPressOut = () => {
-    this.setState({
-      buttonPressed: false
-    })
-    this.props.navigation.navigate("Profile")
-  }
+  const handleButtonPressOut = () => {
+    setButtonPressed(false);
+    props.navigation.navigate("Profile")
+  };
   
-  validateInput = () => {
+  const validateInput = () => {
     let nameValid = false;
     let emailValid = false;
 
@@ -58,42 +34,40 @@ export default class OnboardingScreen extends React.Component {
     const name_re = /^[a-z ]+$/i
     const email_re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
 
-    nameValid = name_re.test(this.state.firstName);
-    emailValid = email_re.test(this.state.email);
+    nameValid = name_re.test(firstName);
+    emailValid = email_re.test(email);
 
     return nameValid && emailValid;
-  }
+  };
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <View style={styles.headerContainer}>
-        </View>
-
-        <View style={styles.contentContainer}>
-          <Text style={styles.heading}>Let us get to know you!</Text>
-          <KeyboardAwareScrollView style={styles.formContainer}>
-            <View style={styles.inputContainer}>
-              <Text style={styles.label}>First Name</Text>
-              <TextInput style={styles.inputField} onChangeText={this.handleNameChange} value={this.state.firstName} />
-            </View>
-            
-            <View style={styles.inputContainer}>
-              <Text style={styles.label}>Email</Text>
-              <TextInput style={styles.inputField} onChangeText={this.handleEmailChange} value={this.state.email} />
-            </View>
-
-            <Pressable
-              style={!this.state.inputValid ? styles.buttonDisabled : this.state.buttonPressed ? styles.buttonPressed : styles.button}
-              onPressIn={this.handleButtonPressIn}
-              onPressOut={this.handleButtonPressOut}
-              disabled={!this.state.inputValid}
-            >
-              <Text style={styles.buttonText}>Next</Text>
-            </Pressable>
-          </KeyboardAwareScrollView>
-        </View>
+  return (
+    <View style={styles.container}>
+      <View style={styles.headerContainer}>
       </View>
-    );
-  }
+
+      <View style={styles.contentContainer}>
+        <Text style={styles.heading}>Let us get to know you!</Text>
+        <KeyboardAwareScrollView style={styles.formContainer}>
+          <View style={styles.inputContainer}>
+            <Text style={styles.label}>First Name</Text>
+            <TextInput style={styles.inputField} onChangeText={setFirstName} />
+          </View>
+          
+          <View style={styles.inputContainer}>
+            <Text style={styles.label}>Email</Text>
+            <TextInput style={styles.inputField} onChangeText={setEmail} />
+          </View>
+
+          <Pressable
+            style={!inputValid ? styles.buttonDisabled : buttonPressed ? styles.buttonPressed : styles.button}
+            onPressIn={handleButtonPressIn}
+            onPressOut={handleButtonPressOut}
+            disabled={!inputValid}
+          >
+            <Text style={styles.buttonText}>Next</Text>
+          </Pressable>
+        </KeyboardAwareScrollView>
+      </View>
+    </View>
+  );
 }
--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,99 +1,119 @@
-import React from 'react';
+import { useState } from 'react';
 import { View, Text, TextInput, Pressable } from "react-native";
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
 import BouncyCheckbox from "react-native-bouncy-checkbox";
 
 import styles from '../styles';
 
-export default class ProfileScreen extends React.Component {
-  state = {
-    isSelected: true,
-    logOutPressed: false,
-    cancelPressed: false,
-    savePressed: false
-  }
+export default function ProfileScreen() {
+  const [cancelPressed, setCancelPressed] = useState(false);
+  const [savePressed, setSavePressed] = useState(false);
+  const [logoutPressed, setLogoutPressed] = useState(false);
 
-  render() {
-    return (
-        <View style={styles.container}>
-          <View style={styles.headerContainer}>
-          </View>
+  const handleCancelPressIn = () => {
+    setCancelPressed(true);
+  };
+  const handleCancelPressOut = () => {
+    setCancelPressed(false);
+  };
+  const handleSavePressIn = () => {
+    setSavePressed(true);
+  };
+  const handleSavePressOut = () => {
+    setSavePressed(false);
+  };
+  const handleLogoutPressIn = () => {
+    setLogoutPressed(true);
+  };
+  const handleLogoutPressOut = () => {
+    setLogoutPressed(false);
+  };
 
-          <View style={styles.contentContainer}>
-            <Text style={styles.heading}>Account Settings</Text>
-            <KeyboardAwareScrollView>
-              <Text style={styles.labelSmall}>First Name</Text>
-              <TextInput style={styles.inputField} />
-              <Text style={styles.labelSmall}>Last Name</Text>
-              <TextInput style={styles.inputField} />
-              <Text style={styles.labelSmall}>Email</Text>
-              <TextInput style={styles.inputField} />
-              <Text style={styles.labelSmall}>Phone Number</Text>
-              <TextInput style={styles.inputField} keyboardType="numeric" />
-              <Text style={styles.labelSmall}>Email Notifications</Text>
-              <BouncyCheckbox
-                style={styles.inputContainer}
-                textStyle={{
-                  textDecorationLine: "none",
-                }}
-                text="Order Statuses"
-                size={25}
-                fillColor="#33401c"
-                unfillColor="#FFFFFF"
-                innerIconStyle={{ borderWidth: 2 }}
-              />
-              <BouncyCheckbox
-                style={styles.inputContainer}
-                textStyle={{
-                  textDecorationLine: "none",
-                }}
-                text="Password Changes"
-                size={25}
-                fillColor="#33401c"
-                unfillColor="#FFFFFF"
-                innerIconStyle={{ borderWidth: 2 }}
-              />
-              <BouncyCheckbox
-                style={styles.inputContainer}
-                textStyle={{
-                  textDecorationLine: "none",
-                }}
-                text="Special Offers"
-                size={25}
-                fillColor="#33401c"
-                unfillColor="#FFFFFF"
-                innerIconStyle={{ borderWidth: 2 }}
-              />
-              <BouncyCheckbox
-                style={styles.inputContainer}
-                textStyle={{
-                  textDecorationLine: "none",
-                }}
-                text="Newsletter"
-                size={25}
-                fillColor="#33401c"
-                unfillColor="#FFFFFF"
-                innerIconStyle={{ borderWidth: 2 }}
-              />
-
-              <Pressable
-                style={this.state.cancelPressed ? styles.buttonPressed : styles.button}
-              >
-                <Text style={styles.buttonText}>Discard Changes</Text>
-              </Pressable>
-              <Pressable
-                style={this.state.savePressed ? styles.buttonPressed : styles.button}
-              >
-                <Text style={styles.buttonText}>Save Changes</Text>
-              </Pressable>
-              <Pressable
-                style={this.state.logOutPressed ? styles.buttonPressed : styles.button}
-              >
-                <Text style={styles.buttonText}>Log Out</Text>
-              </Pressable>
-            </KeyboardAwareScrollView>
-          </View>
+  return (
+      <View style={styles.container}>
+        <View style={styles.headerContainer}>
         </View>
-    );
-  }
+
+        <View style={styles.contentContainer}>
+          <Text style={styles.heading}>Account Settings</Text>
+          <KeyboardAwareScrollView>
+            <Text style={styles.labelSmall}>First Name</Text>
+            <TextInput style={styles.inputField} />
+            <Text style={styles.labelSmall}>Last Name</Text>
+            <TextInput style={styles.inputField} />
+            <Text style={styles.labelSmall}>Email</Text>
+            <TextInput style={styles.inputField} />
+            <Text style={styles.labelSmall}>Phone Number</Text>
+            <TextInput style={styles.inputField} keyboardType="numeric" />
+            <Text style={styles.labelSmall}>Email Notifications</Text>
+            <BouncyCheckbox
+              style={styles.inputContainer}
+              textStyle={{
+                textDecorationLine: "none",
+              }}
+              text="Order Statuses"
+              size={25}
+              fillColor="#33401c"
+              unfillColor="#FFFFFF"
+              innerIconStyle={{ borderWidth: 2 }}
+            />
+            <BouncyCheckbox
+              style={styles.inputContainer}
+              textStyle={{
+                textDecorationLine: "none",
+              }}
+              text="Password Changes"
+              size={25}
+              fillColor="#33401c"
+              unfillColor="#FFFFFF"
+              innerIconStyle={{ borderWidth: 2 }}
+            />
+            <BouncyCheckbox
+              style={styles.inputContainer}
+              textStyle={{
+                textDecorationLine: "none",
+              }}
+              text="Special Offers"
+              size={25}
+              fillColor="#33401c"
+              unfillColor="#FFFFFF"
+              innerIconStyle={{ borderWidth: 2 }}
+            />
+            <BouncyCheckbox
+              style={styles.inputContainer}
+              textStyle={{
+                textDecorationLine: "none",
+              }}
+              text="Newsletter"
+              size={25}
+              fillColor="#33401c"
+              unfillColor="#FFFFFF"
+              innerIconStyle={{ borderWidth: 2 }}
+            />
+
+            <Pressable
+              style={cancelPressed ? styles.buttonPressed : styles.button}
+              onPressIn={handleCancelPressIn}
+              onPressOut={handleCancelPressOut}
+            >
+              <Text style={styles.buttonText}>Discard Changes</Text>
+            </Pressable>
+            <Pressable
+              style={savePressed ? styles.buttonPressed : styles.button}
+              onPressIn={handleSavePressIn}
+              onPressOut={handleSavePressOut}
+            >
+              <Text style={styles.buttonText}>Save Changes</Text>
+            </Pressable>
+            <Pressable
+              style={logoutPressed ? styles.buttonPressed : styles.button}
+              onPressIn={handleLogoutPressIn}
+              onPressOut={handleLogoutPressOut}
+            >
+              <Text style={styles.buttonText}>Log Out</Text>
+            </Pressable>
+          </KeyboardAwareScrollView>
+        </View>
+      </View>
+  );
 }
